4533f663cf47d3909e7c551192499d347fd0438d
NaseerAhmed7659 <shaiknaseerahammed978@gmail.com>
2024-11-22 15:01:01 +0530

    Update p1.js

--- p1.js
+++ p1.js
@@ -17,18 +17,13 @@ async function fun(val) {
 
     let response =await fetch('https://restcountries.com/v3.1/name/'+val)
     let re =await fetch('https://restcountries.com/v3.1/name/'+ val)
-  //  let cl = await fetch('https://restcountries.com/v3.1/name/'+val)
-    
-    
+
     let res = await response.json()
     let r = await re.json()
-   // let c=await cl.json() 
-
-    // console.log(Object.values(c[0].altSpellings)[0])
+   
     console.log(Object.values(res[0].currencies)[0].name)
     console.log(Object.values(res[0].capital))
-    
-   // co.innerHTML = 'Name:- '+ Object.values(c[0].altSpellings)[0]
+
     cu.innerHTML = 'Capital:- '+ Object.values(res[0].capital)
     value.innerHTML = 'Currency :- '+ Object.values(res[0].currencies)[0].name 
     sy.innerHTML = 'Symbol:- '+ Object.values(res[0].currencies)[0].symbol
